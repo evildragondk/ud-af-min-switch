d33a6d260c1618d5501c7e955d2330ac5ab8b1db
evildragondk <59565401+evildragondk@users.noreply.github.com>
2020-03-20 12:53:49 +0100

    conflit 1

---
+++ 1-10/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace _1_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dit_tal = new System.Windows.Forms.Label();
+            this.hvad_er_tal = new System.Windows.Forms.Label();
+            this.en = new System.Windows.Forms.Button();
+            this.to = new System.Windows.Forms.Button();
+            this.tre = new System.Windows.Forms.Button();
+            this.fire = new System.Windows.Forms.Button();
+            this.fem = new System.Windows.Forms.Button();
+            this.seks = new System.Windows.Forms.Button();
+            this.sye = new System.Windows.Forms.Button();
+            this.otto = new System.Windows.Forms.Button();
+            this.ni = new System.Windows.Forms.Button();
+            this.ti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dit_tal
+            // 
+            this.dit_tal.AutoSize = true;
+            this.dit_tal.Location = new System.Drawing.Point(145, 117);
+            this.dit_tal.Name = "dit_tal";
+            this.dit_tal.Size = new System.Drawing.Size(191, 13);
+            this.dit_tal.TabIndex = 0;
+            this.dit_tal.Text = "har skal det tal du tro der er det riget tal";
+            // 
+            // hvad_er_tal
+            // 
+            this.hvad_er_tal.AutoSize = true;
+            this.hvad_er_tal.Location = new System.Drawing.Point(495, 117);
+            this.hvad_er_tal.Name = "hvad_er_tal";
+            this.hvad_er_tal.Size = new System.Drawing.Size(326, 13);
+            this.hvad_er_tal.TabIndex = 1;
+            this.hvad_er_tal.Text = "har vil den fortælle om det tal du har vlat er forhjørt forlaver eller riget";
+            // 
+            // en
+            // 
+            this.en.Location = new System.Drawing.Point(12, 179);
+            this.en.Name = "en";
+            this.en.Size = new System.Drawing.Size(75, 75);
+            this.en.TabIndex = 2;
+            this.en.Text = "1";
+            this.en.UseVisualStyleBackColor = true;
+            this.en.Click += new System.EventHandler(this.en_Click);
+            // 
+            // to
+            // 
+            this.to.Location = new System.Drawing.Point(93, 179);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(75, 75);
+            this.to.TabIndex = 3;
+            this.to.Text = "2";
+            this.to.UseVisualStyleBackColor = true;
+            this.to.Click += new System.EventHandler(this.to_Click);
+            // 
+            // tre
+            // 
+            this.tre.Location = new System.Drawing.Point(174, 179);
+            this.tre.Name = "tre";
+            this.tre.Size = new System.Drawing.Size(75, 75);
+            this.tre.TabIndex = 4;
+            this.tre.Text = "3";
+            this.tre.UseVisualStyleBackColor = true;
+            this.tre.Click += new System.EventHandler(this.tre_Click);
+            // 
+            // fire
+            // 
+            this.fire.Location = new System.Drawing.Point(255, 179);
+            this.fire.Name = "fire";
+            this.fire.Size = new System.Drawing.Size(75, 75);
+            this.fire.TabIndex = 5;
+            this.fire.Text = "4";
+            this.fire.UseVisualStyleBackColor = true;
+            this.fire.Click += new System.EventHandler(this.fire_Click);
+            // 
+            // fem
+            // 
+            this.fem.Location = new System.Drawing.Point(336, 179);
+            this.fem.Name = "fem";
+            this.fem.Size = new System.Drawing.Size(75, 75);
+            this.fem.TabIndex = 6;
+            this.fem.Text = "5";
+            this.fem.UseVisualStyleBackColor = true;
+            this.fem.Click += new System.EventHandler(this.fem_Click);
+            // 
+            // seks
+            // 
+            this.seks.Location = new System.Drawing.Point(417, 179);
+            this.seks.Name = "seks";
+            this.seks.Size = new System.Drawing.Size(75, 75);
+            this.seks.TabIndex = 7;
+            this.seks.Text = "6";
+            this.seks.UseVisualStyleBackColor = true;
+            this.seks.Click += new System.EventHandler(this.seks_Click);
+            // 
+            // sye
+            // 
+            this.sye.Location = new System.Drawing.Point(498, 179);
+            this.sye.Name = "sye";
+            this.sye.Size = new System.Drawing.Size(75, 75);
+            this.sye.TabIndex = 8;
+            this.sye.Text = "7";
+            this.sye.UseVisualStyleBackColor = true;
+            this.sye.Click += new System.EventHandler(this.sye_Click);
+            // 
+            // otto
+            // 
+            this.otto.Location = new System.Drawing.Point(579, 179);
+            this.otto.Name = "otto";
+            this.otto.Size = new System.Drawing.Size(75, 75);
+            this.otto.TabIndex = 9;
+            this.otto.Text = "8";
+            this.otto.UseVisualStyleBackColor = true;
+            this.otto.Click += new System.EventHandler(this.otto_Click);
+            // 
+            // ni
+            // 
+            this.ni.Location = new System.Drawing.Point(660, 179);
+            this.ni.Name = "ni";
+            this.ni.Size = new System.Drawing.Size(75, 75);
+            this.ni.TabIndex = 10;
+            this.ni.Text = "9";
+            this.ni.UseVisualStyleBackColor = true;
+            this.ni.Click += new System.EventHandler(this.ni_Click);
+            // 
+            // ti
+            // 
+            this.ti.Location = new System.Drawing.Point(741, 179);
+            this.ti.Name = "ti";
+            this.ti.Size = new System.Drawing.Size(75, 75);
+            this.ti.TabIndex = 11;
+            this.ti.Text = "10";
+            this.ti.UseVisualStyleBackColor = true;
+            this.ti.Click += new System.EventHandler(this.ti_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 260);
+            this.Controls.Add(this.ti);
+            this.Controls.Add(this.ni);
+            this.Controls.Add(this.otto);
+            this.Controls.Add(this.sye);
+            this.Controls.Add(this.seks);
+            this.Controls.Add(this.fem);
+            this.Controls.Add(this.fire);
+            this.Controls.Add(this.tre);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.en);
+            this.Controls.Add(this.hvad_er_tal);
+            this.Controls.Add(this.dit_tal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label dit_tal;
+        private System.Windows.Forms.Label hvad_er_tal;
+        private System.Windows.Forms.Button en;
+        private System.Windows.Forms.Button to;
+        private System.Windows.Forms.Button tre;
+        private System.Windows.Forms.Button fire;
+        private System.Windows.Forms.Button fem;
+        private System.Windows.Forms.Button seks;
+        private System.Windows.Forms.Button sye;
+        private System.Windows.Forms.Button otto;
+        private System.Windows.Forms.Button ni;
+        private System.Windows.Forms.Button ti;
+    }
+}
+
---
+++ 1-10/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void en_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void to_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void tre_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void fire_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void fem_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void seks_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void sye_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void otto_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void ni_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+
+        private void ti_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            dit_tal.Text = dit_tal.Text + button.Text;
+        }
+    }
+}
